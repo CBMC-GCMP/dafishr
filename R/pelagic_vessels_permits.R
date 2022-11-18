dbfaf4e2b70b4df876118e64e838ee847adbceb6
Fabbiologia <favoretto.fabio@gmail.com>
2022-11-18 21:46:55 +0100

    eliminated large data and added permits

---
+++ R/pelagic_vessels_permits.R
@@ -0,0 +1,13 @@
+#' List of vessels with pelagic fishing permits
+#'
+#' A `data.frame` object extracted from a raw dataset of permits available under request at dataMares (https://datamares.org/)
+#'
+#' @format A `data.frame` with 719 rows and 2 columns.
+#' \describe{
+#'   \item{RNP}{Unique code identifing the vessel}
+#'   \item{vessel_name}{Name of the vessel}
+#'   ...
+#' }
+#' @source \url{www.datamares.org}
+
+"pelagic_vessels_permits"
