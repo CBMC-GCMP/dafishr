fbe8a5c1943bbca0d010b87af367170a54530fed
Fabbiologia <favoretto.fabio@gmail.com>
2022-11-29 12:56:30 +0100

    improved name of file path argument, added a destination folder argument, removed unused dependency corrected example, improved message management

--- R/preprocessing_vms.R
+++ R/preprocessing_vms.R
@@ -9,15 +9,16 @@
 #'
 #'
 #'
-#' @param path it can be a path to the file downloaded or the data object itself.
+#' @param files.path it can be a path to the file downloaded or the data object itself.
 #' If function is used with a path it adds a `file` column to the returning data.frame object that
 #' stores the name of the file as a reference.
+#' @param destination.folder it must record the path to a folder were all the preprocessed files
+#' will be stored.
 #'
 #'
 #' @return A `.fst` file saved within a `preprocessed` directory automatically created that stores
 #' each of the files that are used as input to the function.
 #' @export
-#' @import DiagrammeR
 #' @import ggplot2
 #' @import readxl
 #' @import tibble
@@ -31,7 +32,7 @@
 #'
 #' # An example on downloaded data
 #' \dontrun{
-#' vms_download(2019) # downloads raw data from 2019 with standard subfolders
+#' vms_download(year = 2019, destination.folder = getwd()) # downloads raw data from 2019 with standard subfolders
 #'
 #' preprocessing_vms("VMS-data/RLMSEP_2019/1.-ENERO/01-31 ENE 2008.csv")
 #' }
@@ -54,25 +55,62 @@
 #'   future_lapply(file_names, preprocessing_vms)
 #' )
 #' }
-preprocessing_vms <- function(path) {
-  mx_inland <- dafishr::mx_inland
-  mx_ports <- dafishr::mx_ports
-  all_mpas <- dafishr::all_mpas
-  vms <- vms_clean(path)
+preprocessing_vms <- function(files.path, destination.folder) {
+            if (missing(destination.folder)) {
+                        stop(
+                                    "destination.folder argument must be specified with an existing path"
+                        )
+            }
 
-  # Intersect with landmass to filter nonsensical coordinates
+            mx_inland <- dafishr::mx_inland
+            mx_ports <- dafishr::mx_ports
+            all_mpas <- dafishr::all_mpas
+            suppressMessages({
+            suppressWarnings({
+            vms <- vms_clean(files.path)
 
-  vms <- clean_land_points(vms, mx_inland)
+            # Intersect with landmass to filter nonsensical coordinates
 
-  # Intersect with ports buffer and create a column "port_visit" at port or at sea
-  vms <- join_ports_locations(vms, mx_ports)
+            vms <- clean_land_points(vms, mx_inland)
 
-  # Intersect with MX MPAs and create a column with MPA name or OA
-  vms <- join_mpa_data(vms, all_mpas)
+            # Intersect with ports buffer and create a column "port_visit" at port or at sea
+            vms <- join_ports_locations(vms, mx_ports)
+
+            # Intersect with MX MPAs and create a column with MPA name or OA
+            vms <- join_mpa_data(vms, all_mpas)
 
 
-  dir.create("preprocessed/")
-
-  fst::write_fst(vms, paste0("preprocessed/vms_", unique(vms$year), "_", unique(vms$month), "_", min(vms$day), "_", max(vms$day), "_preprocessed.fst"))
-  cat(paste0("Writing file: vms_", unique(vms$year), "_", unique(vms$month), "_", min(vms$day), "_", max(vms$day), "_preprocessed.fst"))
+            dir.create(destination.folder)
+            })
+            })
+            fst::write_fst(
+                        vms,
+                        paste0(
+                                    destination.folder,
+                                    "/vms_",
+                                    unique(vms$year),
+                                    "_",
+                                    unique(vms$month),
+                                    "_",
+                                    min(vms$day),
+                                    "_",
+                                    max(vms$day),
+                                    "_preprocessed.fst"
+                        )
+            )
+            message(
+                        paste0(
+                                    "Writing file: vms_",
+                                    unique(vms$year),
+                                    "_",
+                                    unique(vms$month),
+                                    "_",
+                                    min(vms$day),
+                                    "_",
+                                    max(vms$day),
+                                    "_preprocessed.fst in the ",
+                                    destination.folder,
+                                    " folder"
+                        )
+            )
 }
